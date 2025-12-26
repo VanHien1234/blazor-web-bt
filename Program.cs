59d0db70a0e1761f5ebf9291ff9571dd1f7541ce
HienNv59 <hiennv59@fpt.com>
2025-12-26 14:14:11 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+//build
+builder.Services.AddRazorPages(); // DI thư viện razor web
+builder.Services.AddServerSideBlazor(); //DI thư viên server side 
+
+//DI service http : dùng để gọi từ server blazor đến server khác để lấy dữ liệu
+builder.Services.AddHttpClient();
+
+
+
+var app = builder.Build();
+//use: Sử dụng thư viện
+app.UseHttpsRedirection(); //https 
+
+
+app.MapBlazorHub(); //middleware của blazor để làm file chạy đầu tiên
+app.MapFallbackToPage("/_Host"); //File chọn chạy đầu tiên
+
+app.UseStaticFiles(); // middleware để sử dụng file tĩnh như css, js, img
+
+// Ví dụ : localhost:5000/products/black-car.jpg
+
+app.Run(); // web được start
+
